0ed72d84e4261801d34b7699a6c6de3c7a687793
Mark <2831694483@qq.com>
2025-01-28 15:13:45 +0800

    保留一个pwm限幅,提高遥控最大速度为15m/s

--- code/uartReceiver.c
+++ code/uartReceiver.c
@@ -16,7 +16,7 @@ void Speed_map(void)
     // 线性插值计算目标速度
     float target_speed;
 
-    target_speed = (10.0 / (1824 - 1024)) * (channel_value - 1024);
+    target_speed = (15.0 / (1824 - 1024)) * (channel_value - 1024);
 
     // 设置 PID 目标速度
     PID_OK(target_speed);
